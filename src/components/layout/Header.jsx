ce8237af5e2c6a52ecefb2fc950bfa1eb4bfaed3
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2025-12-23 21:33:13 -0500

    update Header component to change Zap icon fill color to white

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,7 +4,7 @@ const Header = ({ onOpenSettings }) => (
   <header style={{ display: 'flex', justifyContent: 'space-between', padding: '1.5rem 2rem', maxWidth: '1200px', margin: '0 auto', position: 'relative', zIndex: 10 ,left:'350px'}}>
     <div style={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
       <div style={{ background: 'var(--primary-purple)', padding: '6px', borderRadius: '8px', display: 'flex' }}>
-        <Zap size={18} color="white" fill="#EDE8D0" />
+        <Zap size={18} color="white" fill="white" />
       </div>
       <h1 style={{ fontSize: '1.25rem', fontWeight: 700, margin: 0 }}>Zenith Timer</h1>
     </div>
